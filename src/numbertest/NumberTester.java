bf7d265216fa8ad3f1dfa8305bf7c8c5d9518b96
Daniel Lehner <daniel.lehner030303@gmail.com>
2019-09-27 10:14:16 +0200

    initialize project

---
+++ src/numbertest/NumberTester.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package numbertest;
+
+/**
+ *
+ * @author dlehner1703
+ */
+public class NumberTester {
+
+}
